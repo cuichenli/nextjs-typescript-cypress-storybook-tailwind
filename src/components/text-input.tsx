73c406049c0697510a28202d7bbe78c0b27c273f
Cuichen Li <cuichen.li94@gmail.com>
2020-10-19 10:35:34 +1100

    Add a simple component

---
+++ src/components/text-input.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const TextInputComponent: React.FunctionComponent = () => {
+  return (
+    <div>
+      <input placeholder="type"></input>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import { TextInputComponent } from "../components/text-input"
+
+export default function Home() {
+  return (
+    <Fragment>
+      <p>Hello</p>
+      <TextInputComponent />
+    </Fragment>
+  )
+}
