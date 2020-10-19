b02ac3f17001b5e8bb70ff463729a9de1aeec071
Cuichen Li <cuichen.li94@gmail.com>
2020-10-19 13:25:08 +1100

    Add style for text-input component

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -3,7 +3,10 @@ import React from "react"
 export const TextInputComponent: React.FunctionComponent = () => {
   return (
     <div>
-      <input placeholder="type"></input>
+      <input
+        placeholder="type"
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      ></input>
     </div>
   )
 }
